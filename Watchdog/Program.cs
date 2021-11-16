3cc1b65b669a966f1e8a9468c58296ddfbe90b8e
Matteo Beltrame <bltmtt@gmail.com>
2021-11-16 23:59:25 +0100

    Initial commit

---
+++ Watchdog/Program.cs
@@ -0,0 +1,46 @@
+﻿//TODO args: name, wake up interval
+using GibNet;
+using GibNet.Logging;
+using System.Diagnostics;
+
+ArgumentsHandler handler = ArgumentsHandler.Factory()
+    .Positional("process")
+    .Positional("arguments")
+    .Keyed("-w", "working directory")
+    .Keyed("-mi", "max number of reboots")
+    .Flag("/e", "embed the watchdog in a single terminal")
+    .Flag("/k", "kill the observed process when the watchdog is terminated")
+    .Build();
+handler.LoadArgs(args);
+if (handler.Valid())
+{
+    handler.GetKeyed("-w", out string workingDir);
+    string executable = handler.GetPositional(0);
+    string arguments = handler.GetPositional(1);
+    Watchdog watchdog = new Watchdog(executable, arguments, workingDir)
+    {
+        Embed = handler.HasFlag("/e"),
+        KillOnExit = handler.HasFlag("/k")
+    };
+    if (handler.GetKeyed("-mi", out string iters))
+    {
+        try
+        {
+            int val = int.Parse(iters);
+            watchdog.MaxIterations = val;
+        }
+        catch (Exception)
+        {
+            Logger.ConsoleInstance.LogError("-mi requires an int");
+            Console.ReadKey();
+            Environment.Exit(1);
+        }
+    }
+    await watchdog.Loop();
+}
+else
+{
+    Logger.ConsoleInstance.LogError("Wrong synthax, args: \n" + handler.ToString());
+    Console.ReadKey();
+    Environment.Exit(1);
+}
---
+++ Watchdog/Watchdog.cs
@@ -0,0 +1,88 @@
+﻿using GibNet.Logging;
+using System.Diagnostics;
+
+public class Watchdog
+{
+    private readonly string executablePath;
+    private readonly string arguments;
+    private readonly string? workingDir;
+    private readonly string executableName;
+    private int iterationCounter = 0;
+    private Process? instance;
+
+    public bool Inhibit { get; private set; } = false;
+
+    public bool Embed { get; set; } = false;
+
+    public bool KillOnExit { get; set; } = false;
+
+    public int MaxIterations { get; set; } = int.MaxValue;
+
+    public Watchdog(string executable, string arguments, string? workingDir = null)
+    {
+        this.executablePath = executable;
+        this.arguments = arguments;
+        this.workingDir = workingDir;
+        this.executableName = SplitPath(executable).Item2;
+        AppDomain.CurrentDomain.ProcessExit += OnExit;
+    }
+
+    public async Task Loop()
+    {
+        Logger.ConsoleInstance.LogInfo("Booting " + executableName);
+        while (!Inhibit && iterationCounter < MaxIterations)
+        {
+            instance = StartProcess();
+            if (instance != null)
+            {
+                await instance.WaitForExitAsync();
+                iterationCounter++;
+                Logger.ConsoleInstance.LogWarning("Iteration: " + iterationCounter + " | " + executableName + " has stopped with exit code: " + instance.ExitCode + ", rebooting");
+            }
+        }
+    }
+
+    private void OnExit(object? sender, EventArgs args)
+    {
+        if (KillOnExit && instance != null)
+        {
+            instance.Kill();
+        }
+    }
+
+    private (string, string) SplitPath(string path)
+    {
+        var index = path.LastIndexOf("/");
+        if (index == -1)
+        {
+            index = path.LastIndexOf("\\");
+        }
+        var folder = index < 0 ? string.Empty : path[..(index + 1)];
+        var name = path.Substring(index + 1, path.Length - index - 1);
+        return (folder, name);
+    }
+
+    private Process? StartProcess()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo(executablePath, arguments);
+        if (!Embed)
+        {
+            startInfo.UseShellExecute = true;
+        }
+        if (workingDir != null)
+        {
+            startInfo.WorkingDirectory = workingDir;
+        }
+
+        return Process.Start(startInfo);
+    }
+
+    public class WatchdogLogger : Logger
+    {
+        public override void LogError(string log, bool newLine = true) => base.LogError(log, newLine);
+
+        public override void LogInfo(string log, ConsoleColor color, bool newLine = true) => base.LogInfo("(Watchdog) " + log, color, newLine);
+
+        public override void LogWarning(string log, bool newLine = true) => base.LogWarning(log, newLine);
+    }
+}
